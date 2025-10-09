6197d0fa31c5733409455fb06c4f658e267a6f8e
Jaden Singh <jadens@andrew.cmu.edu>
2025-10-09 18:35:51 -0400

    menu opener function added back!

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,6 +77,25 @@
 				});
 			}
 
+			$menu_openers.each(function() {
+
+				var $this = $(this);
+
+				$this.on('click', function(event) {
+
+					// Prevent default.
+						event.preventDefault();
+
+					// Toggle.
+						$menu_openers.not($this).removeClass('active');
+						$this.toggleClass('active');
+
+					// Trigger resize (sidebar lock).
+						$window.triggerHandler('resize.sidebar-lock');
+
+				});
+			});
+
 		})
 		.catch(error => console.error("Error loading sidebar:", error));
 
